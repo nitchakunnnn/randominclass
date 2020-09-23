8bc97b5c871bb24d718a57044c4b85604e522810
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2020-09-23 21:38:49 +0700

    sqnc-index

--- js/sequence.js
+++ js/sequence.js
@@ -1,7 +1,8 @@
 function randomFunc() {
 	let result = document.getElementById("result");
 	let quantity = document.getElementById("quantityStudents").value;
-	let checkBox = document.getElementById("checkCondition").checked;
+	let isCheckBox = document.getElementById("checkCondition").checked;
+	let iSshowIndex = document.getElementById("showIndex").checked;
 	
 	result.value = '';
 	if(quantity == '')
@@ -12,23 +13,45 @@ function randomFunc() {
 	let arrayendSequence = (document.getElementById("endSequence").value).split(',').map(x=>+x);
 	let unuseSequence = (document.getElementById("unuseSequence").value).split(',').map(x=>+x);
 	
-	if(checkBox){
+	if(isCheckBox){
+		let ind = 1;
 		arraySequence = arraySequence.filter(x => !arraystartSequence.includes(x) );
 		arraySequence = arraySequence.filter(x => !arrayendSequence.includes(x) );
 		arraySequence = arraySequence.filter(x => !unuseSequence.includes(x) );
 		for(let i = 0 ; i < arraystartSequence.length ; i++){
-			result.value += arraystartSequence[i] + '\n';
+			if(iSshowIndex){
+				result.value += ind.toString() + '. ' + arraystartSequence[i] + '\n';
+				ind++;
+			}
+			else
+				result.value += arraystartSequence[i] + '\n';
 		}
 		for(let i = 0 ; i < arraySequence.length ; i++){
-			result.value += arraySequence[i] + '\n';
+			if(iSshowIndex){
+				result.value += ind.toString() + '. ' + arraySequence[i] + '\n';
+				ind++;
+			}
+			else
+				result.value += arraySequence[i] + '\n';
 		}
 		for(let i = 0 ; i < arrayendSequence.length ; i++){
-			result.value += arrayendSequence[i] + '\n';
+			if(iSshowIndex){
+				result.value += ind.toString() + '. ' + arrayendSequence[i] + '\n';
+				ind++;
+			}
+			else
+				result.value += arrayendSequence[i] + '\n';
 		}
 	}
 	else{
+		let ind = 1;
 		for(let i = 0 ; i < arraySequence.length ; i++){
-			result.value += arraySequence[i] + '\n';
+			if(iSshowIndex){
+				result.value += ind.toString() + '. ' + arraySequence[i] + '\n';
+				ind++;
+			}
+			else
+				result.value += arraySequence[i] + '\n';
 		}
 	}
 }// JavaScript Document// JavaScript Document
