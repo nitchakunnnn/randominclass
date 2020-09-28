9119e19138da842cf606dd3c3d1f0399efe41dde
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2020-09-28 11:40:08 +0700

    qqq

--- js/seat.js
+++ js/seat.js
@@ -1,3 +1,10 @@
-function roomFunc(){
+function setTableFunc(){
+	let result = document.getElementById("result");
+	let quantity = document.getElementById("quantityStudents").value;
+	let isCheckBox = document.getElementById("checkCondition").checked;
+	let iSshowIndex = document.getElementById("showIndex").checked;
+}
+
+function randomFunc(){
     
 }
