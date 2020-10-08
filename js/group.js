f6621d4ec9fd8e85232884e6cb24143d5f99b1dc
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2020-10-08 17:38:38 +0700

    men

--- js/group.js
+++ js/group.js
@@ -1,25 +1,29 @@
+var quantityStudentsInt = 24;
+var quantityGroupsInt = 6;
 function randomFunc(){
 	let result = document.getElementById("result");
 	let quantityStudents = document.getElementById("quantityStudents").value;
 	let quantityGroups = document.getElementById("quantityGroups").value;
 	let sameGroup = document.getElementById("sameGroup").value;
 	let notSameGroup = document.getElementById("notSameGroup").value;
-	let isCheckBox = document.getElementById("checkCondition").checked;
-	let isShowIndex = document.getElementById("showIndex").checked;
+	let menn = document.getElementById("men").value;
 	
-	let quantityStudentsInt = parseInt(quantityStudents)
-	let quantityGroupsInt = parseInt(quantityGroups)
+	quantityStudentsInt = parseInt(quantityStudents);
+	quantityGroupsInt = parseInt(quantityGroups);
+	var men = parseInt(menn);
 	
 	result.value = '';
 	if(quantityStudents == '')
 		quantityStudentsInt = 24;
 	if(quantityGroups == '')
 		quantityGroupsInt = 6;
+	if(menn == '')
+		men = 9;
 	
-	let arraySequence = getRandArray(quantityStudentsInt,quantityGroupsInt);
+	let arraySequence = getRandArray();
 	
 	
-	if(isCheckBox){
+	if(document.getElementById("checkCondition").checked){
 		
 		sameGroup = sameGroup.slice(1, -1);
 		sameGroup = sameGroup.split('),(');
@@ -33,11 +37,11 @@ function randomFunc(){
 			notSameGroup[i] = notSameGroup[i].split(',').map(x=>+x);
 		}
 		
-		while(!isCorrect(arraySequence,sameGroup,notSameGroup))
-			arraySequence = getRandArray(quantityStudentsInt,quantityGroupsInt);
+		while(!isCorrect(arraySequence,sameGroup,notSameGroup,men,quantityGroupsInt))
+			arraySequence = getRandArray();
 	}
 	for(let i = 0 ; i < arraySequence.length ; i++){
-		if(isShowIndex)
+		if(document.getElementById("showIndex").checked)
 			result.value += 'กลุ่มที่ ' + (i+1).toString() + ' :  ';
 		for(let j=0 ; j < arraySequence[i].length; j++){
 			result.value += arraySequence[i][j] + ', ';
@@ -46,7 +50,7 @@ function randomFunc(){
 		result.value += '\n';
 	}
 }// JavaScript Document// JavaScript Document// JavaScript Document
-function getRandArray(quantityStudentsInt,quantityGroupsInt){
+function getRandArray(){
 	let ind = 0;
 	let arraySequence2d = [];
 	let temp = [];
@@ -61,7 +65,7 @@ function getRandArray(quantityStudentsInt,quantityGroupsInt){
 	}
 	return arraySequence2d
 }
-function isCorrect(arraySequence,sameGroup,notSameGroup){
+function isCorrect(arraySequence,sameGroup,notSameGroup,men){
 	for(let i=0;i<arraySequence.length;i++){
 		let setArraySequence = new Set(arraySequence[i]);
 		for(let j=0;j<notSameGroup.length;j++){
@@ -71,7 +75,7 @@ function isCorrect(arraySequence,sameGroup,notSameGroup){
 				return false;
 		}
 	}
-	if(document.getElementById("sameGroup").value!=''){
+	if(document.getElementById("sameGroup").value!='')
 		for(let i=0;i<sameGroup.length;i++){
 			let con = false;
 			for(let j=0;j<arraySequence.length;j++){
@@ -81,6 +85,33 @@ function isCorrect(arraySequence,sameGroup,notSameGroup){
 			if(!con)
 				return false;
 		}
-	}
+	if(document.getElementById("checkMenCondition").checked)
+		for(let i=0;i<arraySequence.length;i++){
+			let qmen = 0;
+			for(let j=0;j<arraySequence[i].length;j++){
+				if(arraySequence[i][j]<=men)
+					qmen++;
+				console.log(arraySequence[i][j]);
+			}
+			if(qmen<Math.floor(men/quantityGroupsInt)){
+				console.log(Math.floor(qmen));
+				console.log(Math.floor(men/quantityGroupsInt));
+				console.log(men);
+				return false;
+			}
+		}
 	return true;
+}
+function checkMenConditionFunc(){
+	// Get the checkbox
+	let checkBox = document.getElementById("checkMenCondition");
+	// Get the output text
+	let div = document.getElementById("menDiv");
+  
+	// If the checkbox is checked, display the output text
+	if (checkBox.checked == true){
+	  div.style.display = "block";
+	} else {
+	  div.style.display = "none";
+	}
 }
